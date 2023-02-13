04ef691b9d6a0b470535beb8c986cb025e1572af
Lucas Clupny <clupnyluke@gmail.com>
2023-02-12 21:27:02 -0500

    add subgrid

--- src/components/Renderer/Renderer.svelte
+++ src/components/Renderer/Renderer.svelte
@@ -6,31 +6,51 @@
 	import { opencascade } from '@Stores';
 	import { solidsToGLB } from '@Utils/solidsToGLB';
 	import { GLTF } from '@threlte/extras';
+	import { merge } from 'lodash';
+
+	const _opts = {
+		backgroundColor: '#333'
+	};
 
 	export let shapes: Promise<TopoDS_Shape | TopoDS_Shape[]>;
+	export let settings: Partial<typeof _opts> = _opts;
+	const opts = merge(_opts, settings);
+	const { backgroundColor } = opts;
 
 	let glbURL: Promise<string> = new Promise(() => null);
+
 	$: glbURL = solidsToGLB($opencascade, shapes);
 	Object3D.DefaultUp.set(0, 0, 1);
 	let axes: AxesHelper;
 	$: if (axes) axes.setColors(new Color(0xff0000), new Color(0x00ff00), new Color(0x0000ff));
+	let target = { x: 0, y: 0.5 };
+	const resetTarget = () => {
+		target = { x: 0, y: 0.5 };
+	};
 </script>
 
-<div class="jscad-renderer">
+<div class="jscad-renderer" style={`background-color: ${backgroundColor};`}>
 	<Canvas>
 		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
 			<OrbitControls
 				enableZoom={true}
-				target={{ x: 0, y: 0.5 }}
+				{target}
 				mouseButtons={{ LEFT: MOUSE.ROTATE, MIDDLE: MOUSE.PAN }}
 			/>
 		</T.PerspectiveCamera>
 
-		<T.DirectionalLight position={[3, 3, 5]} intensity={0.3} />
+		<T.DirectionalLight position={[1, 1, 5]} intensity={0.3} />
 		<T.AmbientLight intensity={1.2} />
 
 		<!-- Floor -->
-		<T.GridHelper args={[210, 210]} rotation.x={degToRad(90)} />
+		<T.GridHelper
+			args={[210, 210, new Color(0xcc2222), new Color(0x666666)]}
+			rotation.x={degToRad(90)}
+		/>
+		<T.GridHelper
+			args={[210, 42, new Color(0xcc2222), new Color(0xcc2222)]}
+			rotation.x={degToRad(90)}
+		/>
 		{#await glbURL then url}
 			<GLTF {url} />
 		{/await}
