ff3cc8eca2af0795b606a04b9db710a04d6cd077
Lucas Clupny <clupnyluke@gmail.com>
2023-02-17 10:42:40 -0500

    remove silly leftover

--- modelingUtils/oc_core/pnt.ts
+++ modelingUtils/oc_core/pnt.ts
@@ -4,7 +4,7 @@ import merge from 'lodash/merge';
 const pnt = (oc: OpenCascadeInstance, opts?: { coords?: [number, number, number] }) => {
 	const _opts = merge({ coords: [0, 0, 0] }, opts);
 	const { coords } = _opts;
-	const [x, y, z] = Array.isArray(coords) ? [...coords, 0, 0] : [coords, 0, 0];
+	const [x, y, z] = coords;
 	return new oc.gp_Pnt_3(x, y, z);
 };
 
