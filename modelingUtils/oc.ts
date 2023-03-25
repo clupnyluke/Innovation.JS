352e166d5281bcffbc177b0026c9214062e2a0b1
Lucas Clupny <clupnyluke@gmail.com>
2023-03-25 12:38:13 -0400

    Revert "oc init for modeling utils"
    This reverts commit e0b797eed5a58c59582ec099f8496ebd5f61dbae.

--- modelingUtils/oc.ts
+++
@@ -1,8 +0,0 @@
-import { OpenCascadeInstance } from "opencascade.js";
-
-// eslint-disable-next-line no-var
-export var oc: OpenCascadeInstance
-
-export const initOpenCascade = (_oc: OpenCascadeInstance) => oc = _oc;
-
-export default oc
