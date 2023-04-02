9fd3d1530b8f33f0316dcc731838aca4a7fb57dd
Lucas Clupny <clupnyluke@gmail.com>
2023-04-02 14:16:39 -0400

    rectangle primitive

---
+++ modelingUtils/primitives/2D/index.ts
@@ -0,0 +1,1 @@
+export { default as rectangle } from './rectangle'
---
+++ modelingUtils/primitives/2D/rectangle.ts
@@ -0,0 +1,20 @@
+import merge from 'lodash/merge';
+import { OpenCascadeInstance } from 'opencascade.js';
+import { fromPoints } from '../wire';
+
+const rectangle = (
+  oc: OpenCascadeInstance,
+  opts?: {
+    size?: [number, number] | [number] | number;
+    origin?: [number, number];
+  }
+) => {
+  const _opts = merge({ size: [1, 1], origin: [0, 0] }, opts);
+  const { origin, size } = _opts;
+  const [x, y] = Array.isArray(size) ? [...size, 1, 1] : [size, size];
+  const [a, b] = origin
+  const points: [number, number, number][] = [[a + x, b + y, 0], [a - x, b + y, 0], [a - x, b - y, 0], [a + x, b - y, 0]]
+  return new oc.BRepBuilderAPI_MakeFace_15(fromPoints(oc, points), true).Face()
+};
+
+export default rectangle
