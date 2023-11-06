5ae4826b697b5f4d54e6ff0304e1a02714f7b79c
Lucas Clupny <clupnyluke@clupny.com>
2023-11-06 12:38:10 -0500

    fix esbuild error crashing server

--- server/socketServer.ts
+++ server/socketServer.ts
@@ -10,54 +10,58 @@ const watcher = new FSWatcher();
 const MODELING_DIR = './modeling/projects';
 
 const emitModel = async (socket: Socket, path: string) => {
-  const module = (
-    await esbuild.build({
-      entryPoints: [`${MODELING_DIR}/${path}.ts`],
-      write: false,
-      platform: 'browser',
-      bundle: true,
-      treeShaking: true,
-      globalName: `module['${path}']`,
-      outfile: 'out',
-      tsconfig: 'server/tsconfig.json',
-    })
-  ).outputFiles[0].text;
-  emit(socket, 'updateModel', { path, module });
+	try {
+		const module = (
+			await esbuild.build({
+				entryPoints: [`${MODELING_DIR}/${path}.ts`],
+				write: false,
+				platform: 'browser',
+				bundle: true,
+				treeShaking: true,
+				globalName: `module['${path}']`,
+				outfile: 'out',
+				tsconfig: 'server/tsconfig.json'
+			})
+		).outputFiles[0].text;
+		emit(socket, 'updateModel', { path, module });
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 const socketServer = async (io: Server) => {
-  const nav = async () => await serverBuildNav(MODELING_DIR, 'Projects');
-  watcher.add(`${MODELING_DIR}/`);
+	const nav = async () => await serverBuildNav(MODELING_DIR, 'Projects');
+	watcher.add(`${MODELING_DIR}/`);
 
-  io.on('connection', (socket) => {
-    watcher.on(
-      'all',
-      async (eventName: 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir', path: string) => {
-        const extension = path.slice(-3);
-        switch (eventName) {
-          default:
-            // TODO: diff fs
-            emit(socket, 'updateNavigation', await nav());
-            break;
-          case 'change':
-            if (extension === '.ts' || extension == '.js') {
-              const relPath = nodePath.relative(MODELING_DIR, path);
-              emit(socket, 'modelUpdated', relPath.slice(0, relPath.length - 3));
-            }
-        }
-      }
-    );
+	io.on('connection', (socket) => {
+		watcher.on(
+			'all',
+			async (eventName: 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir', path: string) => {
+				const extension = path.slice(-3);
+				switch (eventName) {
+					default:
+						// TODO: diff fs
+						emit(socket, 'updateNavigation', await nav());
+						break;
+					case 'change':
+						if (extension === '.ts' || extension == '.js') {
+							const relPath = nodePath.relative(MODELING_DIR, path);
+							emit(socket, 'modelUpdated', relPath.slice(0, relPath.length - 3));
+						}
+				}
+			}
+		);
 
-    emit(socket, 'connected', 'WebSocket connected to server');
+		emit(socket, 'connected', 'WebSocket connected to server');
 
-    listen(socket, 'requestNavigation', async () => {
-      emit(socket, 'updateNavigation', await nav());
-    });
+		listen(socket, 'requestNavigation', async () => {
+			emit(socket, 'updateNavigation', await nav());
+		});
 
-    listen(socket, 'requestModel', async (path) => {
-      emitModel(socket, path);
-    });
-  });
+		listen(socket, 'requestModel', async (path) => {
+			emitModel(socket, path);
+		});
+	});
 };
 
 export default socketServer;
