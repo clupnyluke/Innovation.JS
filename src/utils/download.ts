f82ca01c751fe17df38ae3419f3ad201783d511a
Lucas Clupny <clupnyluke@clupny.com>
2023-12-08 10:11:25 -0500

    make download generic to other file types

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,14 +1,17 @@
-const download = async (fileName: string, { data, url }: { data?: string; url?: string }) => {
+const download = async (
+	fileName: string,
+	{ data, url, type }: { data?: string; url?: string; type?: string }
+) => {
 	let blob: Blob;
 	if (url) blob = await fetch(url).then((res) => res.blob());
-	if (data) blob = new Blob([data], { type: 'model/gltf-binary ' });
+	if (data) blob = new Blob([data], { type: type ?? 'text/plain' });
 	if (!blob) throw new Error('No blob found');
 	const downUrl = URL.createObjectURL(blob);
 	const a = document.createElement('a');
 	a.style.display = 'none';
 	a.href = downUrl;
 	// the filename you want
-	a.download = `${fileName}.glb`;
+	a.download = `${fileName}`;
 	document.body.appendChild(a);
 	a.click();
 	URL.revokeObjectURL(downUrl);
